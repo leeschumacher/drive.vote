3888fd42e0d96fb3e32053c5aea6aa0e2903f19c
Nick Messick <me@nickmessick.com>
2018-09-18 22:09:41 -0700

    Add validator to make sure file is actually a csv/tsv

--- app/models/ride_uploaded_file.rb
+++ app/models/ride_uploaded_file.rb
@@ -2,9 +2,17 @@ class RideUploadedFile < ApplicationRecord
   belongs_to :user
   has_one_attached :uploaded_csv
 
+  validate :uploaded_file_must_be_csv
+
   after_create :enqueue_processing_dry_run
 
   private
+
+  def uploaded_file_must_be_csv
+    if uploaded_csv.attached? && !uploaded_csv.content_type.in?(%w(text/tab-separated-values))
+      errors.add(:uploaded_csv, 'Must be in a comma or tab separated file format')
+    end
+  end
 
   def enqueue_processing_dry_run
     RideUploadedFileProcessingDryRunJob.perform_now(self.id)
